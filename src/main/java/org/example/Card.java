215503ebcf6d2935169950361c001ab0a1429c2e
Dylan-San <drocky73caro@gmail.com>
2024-02-06 19:31:41 -0700

    1st push

---
+++ src/main/java/org/example/Card.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Card implements Comparable<Card> {
+
+    private Suit suit;
+    private FaceValue faceValue;
+
+    private Card() {
+    }
+
+
+    //constructor
+    public Card(Suit suit, FaceValue faceValue) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        return 0;
+    }
+}
